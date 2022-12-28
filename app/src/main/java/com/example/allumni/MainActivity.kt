01418da941d0bbc1e2d26ffb6e7248f8fd1e5528
Briantrhn10 <briyantarihoran@gmail.com>
2022-12-28 19:16:07 +0700

    okeoke

--- app/src/main/java/com/example/allumni/MainActivity.kt
+++ app/src/main/java/com/example/allumni/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.allumni
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.allumni.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigation.setOnItemSelectedListener{
+            when(it.itemId){
+
+                R.id.navigation_home -> replaceFragment(HomeFragment())
+                R.id.navigation_forum -> replaceFragment(ForumFragment())
+                R.id.navigation_notifications -> replaceFragment(NotificationsFragment())
+                R.id.navigation_profil -> replaceFragment(ProfilFragment())
+
+                else->{}
+            }
+            true
+        }
     }
+
+    fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container,fragment)
+        fragmentTransaction.commit()
+    }
+
+
 }
+
